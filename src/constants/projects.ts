a6ecf81a395d4e65b7fc7f970f3c033efa404461
Nic Fan <nicfan@gmail.com>
2023-02-17 02:24:49 -0800

    Added 'Slacklite' to projects

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,7 +1,14 @@
 export const PROJECT_LIST: Array<Project> = [
 	{
+		name: 'Slacklite',
+		desc: 'Chat app built for fun, using Next.js + TypeScript + GraphQL + Node.js + Express.',
+		siteUrl: 'https://slacklite-client.vercel.app',
+		repoUrl: 'https://github.com/nicfan139/slacklite',
+		imgSrc: '/pictures/project_slacklite.png'
+	},
+	{
 		name: 'YAP.cx - Website',
-		desc: 'YAP is a Bitcoin and Foreign currency exchange service based in the Old Port of Montreal.<br/><br/>Designed and built the website from scratch using **React.js + Gatsby.js.**',
+		desc: 'YAP is a Bitcoin and Foreign currency exchange service based in the Old Port of Montreal.<br/><br/>Designed and built the website from scratch using React.js + Gatsby.js.',
 		siteUrl: 'https://yap.cx',
 		imgSrc: '/pictures/project_yap_website.png'
 	},
@@ -19,14 +26,14 @@ export const PROJECT_LIST: Array<Project> = [
 	},
 	{
 		name: 'Gamiflex',
-		desc: "Final project of **Le Wagon Coding Bootcamp** (*batch #125 - Montreal*).<br/><br/>Our team built a webapp that uses a gamer's preferences to suggest their next video game!<br/><br/>This project was bootstrapped using Ruby on Rails in collaboration with Theo Avoyne (@theoavoyne) and Olivier Riccini (@OlivierRiccini).",
+		desc: "Final project of Le Wagon Coding Bootcamp (batch #125 - Montreal).<br/><br/>Our team built a webapp that uses a gamer's preferences to suggest their next video game!<br/><br/>This project was bootstrapped using Ruby on Rails in collaboration with Theo Avoyne (@theoavoyne) and Olivier Riccini (@OlivierRiccini).",
 		siteUrl: 'http://www.gamiflex.com',
 		repoUrl: 'https://github.com/nicfan139/gamiflex',
 		imgSrc: '/pictures/project_gamiflex.png'
 	},
 	{
 		name: 'Montresor',
-		desc: "5-day team project for 'AirBnB Week' at **Le Wagon Coding Bootcamp** (*batch #125 - Montreal*).<br/><br/>We built a marketplace for second-hand items, like Kijiji and Craigslist, but tailored to students.<br/><br/>This project was bootstrapped using Ruby on Rails in collaboration with Theo Avoyne (@theoavoyne) and Olivier Riccini (@OlivierRiccini).",
+		desc: "5-day team project for 'AirBnB Week' at Le Wagon Coding Bootcamp (batch #125 - Montreal).<br/><br/>We built a marketplace for second-hand items, like Kijiji and Craigslist, but tailored to students.<br/><br/>This project was bootstrapped using Ruby on Rails in collaboration with Theo Avoyne (@theoavoyne) and Olivier Riccini (@OlivierRiccini).",
 		siteUrl: 'http://montresor-72.herokuapp.com',
 		repoUrl: 'https://github.com/nicfan139/montresor',
 		imgSrc: '/pictures/project_montresor.png'
